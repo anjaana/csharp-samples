0b07e0afcb0fb31fd77ce4e317e238105435b841
anj <dd-xx90@yandex.ru>
2016-05-11 19:04:00 +0300

    init repository

---
+++ csharp-samples/Interfaces/TryInterfaces.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_samples.Interfaces
+{
+    public static class TryInterfaces
+    {
+        public static void Do()
+        {
+
+        }
+    }
+}
